5c262534519d2dd3d4ffc076a0d9c6dcb7530d22
AashnaSingh09 <123101932@umail.ucc.ie>
2023-11-29 15:00:52 +0000

    Refactored the repeated and redundant code for configuration dialog

--- car10.py
+++ car10.py
@@ -1,5 +1,4 @@
 import sys
-import typing
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import (
     QApplication,
--- constants.py
+++ constants.py
@@ -10,3 +10,17 @@ DEFAULT_BANANA_IMG = 'assets/banana.png'
 DEFAULT_ROADBLOCK_IMG = 'assets/stop.png'
 
 DEFAULT_CAR_IMG = 'assets/car.jpeg'
+
+DEFAULT_SCENARIOS_CHECKED_POSITIONS = [
+    (1, 2), (2, 2), (2, 3), (3, 2), (3, 4),
+    (4, 1), (5, 1), (5, 3), (6, 0), (7, 0),
+    (7, 3), (8, 0), (8, 2)
+]
+
+DEFAULT_COLUMN_HEADERS_FOR_CONFIGURATION_DIALOG = [
+    "Obstacle: Unavoidable",
+    "Obstacle: Avoidable",
+    "Make a Turn",
+    "Other Car: Opposite Direction",
+    "Other Car: Cross Direction"
+]
